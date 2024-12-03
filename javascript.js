7127016a4e0b0c98184a76565e9aa3c9e4d4669c
Cascadenightfox <sydn3y0614@gmail.com>
2024-12-03 14:37:28 +0800

    IsaidMeow

---
+++ javascript.js
@@ -0,0 +1,32 @@
+var quadimages = document.querySelectorAll("#quad figure");
+for(i=0; i<quadimages.length; i++) {
+  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
+}
+
+
+let menuIcon = document.querySelector('menu-icon');
+let navbar = document.querySelector('navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+            });
+            document.querySelector('header nav ul li a[href*="' + id + '"]').classList.add('active');
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
